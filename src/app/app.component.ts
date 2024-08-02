a655c3a56acbd97732cc293c6a21fab850c181c0
Rajinder Singh Sodhi <rajsodhisocial@gmail.com>
2024-08-03 00:28:47 +0530

    Dark mode added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  darkMode: boolean = false;
+
+  toggleMode = (event: Event) => {
+    if(this.darkMode === true){
+      this.darkMode = false;
+    }else{
+      this.darkMode = true;
+    }
+  }
 }
